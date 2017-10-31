7e596e3069d073dc883009384aa0e3467fcd8495
Alex Yang <aleozlx@gmail.com>
2017-10-30 22:57:59 -0500

    bugfix

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from matplotlib import pyplot as plt
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+pattern = (12, 12)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-# Arrays to store object points and image points from all the images.
+objp = np.zeros((np.product(pattern),3), np.float32)
+objp[:,:2] = np.mgrid[:pattern[0],:pattern[1]].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
@@ -18,18 +17,17 @@ images = glob.glob('*.tif')
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-    for pattern in [(12, 12), (12, 13), (13, 12)]:
-        ret, corners = cv2.findChessboardCorners(gray, pattern, None)
-        if ret:
-            objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-            imgpoints.append(corners2)
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, pattern, corners2,ret)
-            cv2.imshow('img', img)
-            cv2.waitKey(500)
-            break
+    ret, corners = cv2.findChessboardCorners(gray, pattern, None)
+    if ret:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, pattern, corners2, ret)
+        cv2.imshow('img', img)
+        cv2.waitKey(500)
 
 cv2.destroyAllWindows()
+
+# http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
+# https://docs.opencv.org/2.4/doc/tutorials/calib3d/camera_calibration/camera_calibration.html
